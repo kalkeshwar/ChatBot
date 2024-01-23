b39190559dd5968e8e36877bbd9853e25eedbc66
kalkeshwar <kalkeshwaryamsani20@gmail.com>
2024-01-23 14:15:01 +0530

    done correct alignment of index.js file

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,7 @@ const express= require("express")
 const cors=require("cors")
 const router = require("./routes")
 const app = express()
-const dotenv= require("dotenv")
-dotenv.config();
+require("dotenv").config()
 const PORT= process.env.PORT || 3001
 
 app.use(express.json())
